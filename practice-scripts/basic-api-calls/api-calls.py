df2dd8dd13e941ecfe9c57c1a3071a20e7fca016
aoheaghra <62123427+aoheaghra@users.noreply.github.com>
2022-01-28 08:07:57 -0500

    added code to the api/scrape scripts

--- practice-scripts/basic-api-calls/api-calls.py
+++ practice-scripts/basic-api-calls/api-calls.py
@@ -1,14 +1,15 @@
 # this is going to be a script for learning api calls
 
 # this is always the section for importing modules
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By 
 
-# Variables
+import requests
 
-DRIVER_PATH = '/usr/local/bin' # this needs to be the path to where you have installed the webdriver google "installing webdrivers for " either firefox or chrome
-URL = "url-goes-here"
+# http://api.open-notify.org/astros.json
+# Logic
+response = requests.get('http://api.open-notify.org/astros.json')
+json = response.json()
+number = str(json['number'])
+people = str(json['people'])
 
-
-# logic of the scraping AKA what are you trying to scrape
+# Executables
+print(number + people)
--- practice-scripts/webscraper/scraper.py
+++ practice-scripts/webscraper/scraper.py
@@ -1,1 +1,22 @@
 # this will be a basic web scraper project
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+
+#URL you are looking to scrape
+URL = "url-goes-here"
+DRIVER_PATH = '/usr/local/bin'
+
+# Scarapping logic
+
+driver = webdriver.Chrome()
+driver.get(URL)
+
+# tempcurrentPrice = driver.find_element(By.CLASS_NAME, "last-change").text
+#element = "location and finding logic here"
+
+
+driver.quit()
