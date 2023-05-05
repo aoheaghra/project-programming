1290347b6e1ef4683c9e524d4b847c77daeeafb5
Waddi08 <waddi08@gmail.com>
2023-05-04 23:25:10 -0400

    Added addition

--- mentee01/calc.py
+++ mentee01/calc.py
@@ -1,1 +1,13 @@
+#varibles
 first = input("First number: ")
+#opperator = input(" + , - , * , /: ")
+second = input("Second number: ")
+
+#data sanitation
+FirstVarible = float(first)
+SecondVarible = float(second)
+
+def sum(FirstVarible, SecondVarible):
+  print(FirstVarible + SecondVarible)
+
+sum(FirstVarible, SecondVarible)
